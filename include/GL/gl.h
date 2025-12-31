aa7c6c14a843f230798d43a9b4123c8491eae4ac
terucam0822 <terucam0822@gmail.com>
2025-12-31 21:54:31 +0900

    Add remaining GL headers (gl.h, freeglut) locally to fix missing headers

---
+++ include/GL/gl.h
@@ -0,0 +1,21 @@
+﻿
+#ifndef __gl_h_
+#define __gl_h_
+
+#ifdef __cplusplus
+extern \
+C\ {
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include <GL/glew.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
