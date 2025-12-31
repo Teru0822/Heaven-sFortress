981ffabd18a4e3e89cced3f8734f5b4669e7184b
terucam0822 <terucam0822@gmail.com>
2025-12-31 21:51:19 +0900

    Replace HTML-contaminated glu.h with clean header content

--- include/GL/glu.h
+++ include/GL/glu.h
@@ -1,42 +1,309 @@
-<!doctype html><html lang="en"><head><title>Making sure you&#39;re not a bot!</title><link rel="stylesheet" href="/.within.website/x/xess/xess.min.css?cachebuster=v1.18.0-11-g9009596"><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="robots" content="noindex,nofollow"><style>
-        body,
-        html {
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            margin-left: auto;
-            margin-right: auto;
-        }
+﻿/*
+ * SGI FREE SOFTWARE LICENSE B (Version 2.0, Sept. 18, 2008)
+ * Copyright (C) 1991-2000 Silicon Graphics, Inc. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the 'Software'),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice including the dates of first publication and
+ * either this permission notice or a reference to http://oss.sgi.com/projects/FreeB/
+ * shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * SILICON GRAPHICS, INC. BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of Silicon Graphics, Inc.
+ * shall not be used in advertising or otherwise to promote the sale, use or
+ * other dealings in this Software without prior written authorization from
+ * Silicon Graphics, Inc.
+ */
 
-        .centered-div {
-            text-align: center;
-        }
+#ifndef __glu_h__
+#define __glu_h__
 
-        #status {
-            font-variant-numeric: tabular-nums;
-        }
+#include <GL/gl.h>
 
-        #progress {
-            display: none;
-            width: min(20rem, 90%);
-            height: 2rem;
-            border-radius: 1rem;
-            overflow: hidden;
-            margin: 1rem 0 2rem;
-            outline-color: #b16286;
-            outline-offset: 2px;
-            outline-style: solid;
-            outline-width: 4px;
-        }
+#ifdef __cplusplus
+extern 'C' {
+#endif
 
-        .bar-inner {
-            background-color: #b16286;
-            height: 100%;
-            width: 0;
-            transition: width 0.25s ease-in;
-        }
-    </style><script id="anubis_version" type="application/json">"v1.18.0-11-g9009596"
-</script><script id="anubis_challenge" type="application/json">{"rules":{"algorithm":"fast","difficulty":3,"report_as":3},"challenge":"1627d117d244eee68e861659f6bb27807ce6464ccddc45b3e2ab37238cdc4bc8"}
-</script><script id="anubis_base_prefix" type="application/json">""
-</script></head><body id="top"><main><center><h1 id="title" class=".centered-div">Making sure you&#39;re not a bot!</h1></center><div class="centered-div"><img id="image" style="width:100%;max-width:256px;" src="/.within.website/x/cmd/anubis/static/img/pensive.webp?cacheBuster=v1.18.0-11-g9009596"> <img style="display:none;" style="width:100%;max-width:256px;" src="/.within.website/x/cmd/anubis/static/img/happy.webp?cacheBuster=v1.18.0-11-g9009596"><p id="status">Loading...</p><script async type="module" src="/.within.website/x/cmd/anubis/static/js/main.mjs?cacheBuster=v1.18.0-11-g9009596"></script><div id="progress" role="progressbar" aria-labelledby="status"><div class="bar-inner"></div></div><details><summary>Why am I seeing this?</summary><p>You are seeing this because the administrator of this website has set up <a href="https://github.com/TecharoHQ/anubis">Anubis</a> to protect the server against the scourge of <a href="https://thelibre.news/foss-infrastructure-is-under-attack-by-ai-companies/">AI companies aggressively scraping websites</a>. This can and does cause downtime for the websites, which makes their resources inaccessible for everyone.</p><p>Anubis is a compromise. Anubis uses a <a href="https://anubis.techaro.lol/docs/design/why-proof-of-work">Proof-of-Work</a> scheme in the vein of <a href="https://en.wikipedia.org/wiki/Hashcash">Hashcash</a>, a proposed proof-of-work scheme for reducing email spam. The idea is that at individual scales the additional load is ignorable, but at mass scraper levels it adds up and makes scraping much more expensive.</p><p>Ultimately, this is a hack whose real purpose is to give a "good enough" placeholder solution so that more time can be spent on fingerprinting and identifying headless browsers (EG: via how they do font rendering) so that the challenge proof of work page doesn't need to be presented to users that are much more likely to be legitimate.</p><p>Please note that Anubis requires the use of modern JavaScript features that plugins like <a href="https://jshelter.org/">JShelter</a> will disable. Please disable JShelter or other such plugins for this domain.</p></details><noscript><p>Sadly, you must enable JavaScript to get past this challenge. This is required because AI companies have changed the social contract around how website hosting works. A no-JS solution is a work-in-progress.</p></noscript><div id="testarea"></div></div><footer><center><p>Protected by <a href="https://github.com/TecharoHQ/anubis">Anubis</a> from <a href="https://techaro.lol">Techaro</a>. Made with ❤️ in 🇨🇦.</p><p>Mascot design by <a href="https://bsky.app/profile/celphase.bsky.social">CELPHASE</a>.</p></center></footer></main></body></html>
+/*****************************************************************************
+ * Extensions
+ *****************************************************************************/
+
+#ifdef __PF_WINDOWS__
+#define GLAPIENTRY __stdcall
+#else
+#define GLAPIENTRY
+#endif
+
+/*****************************************************************************
+ * GLU types
+ *****************************************************************************/
+
+typedef struct GLUnurbs GLUnurbs;
+typedef struct GLUquadric GLUquadric;
+typedef struct GLUtesselator GLUtesselator;
+
+/* backwards compatibility: */
+typedef struct GLUnurbs GLUnurbsObj;
+typedef struct GLUquadric GLUquadricObj;
+typedef struct GLUtesselator GLUtesselatorObj;
+typedef struct GLUtesselator GLUtriangulatorObj;
+
+#define GLU_TESS_MAX_COORD 1.0e150
+
+/* Internal convenience typedefs */
+typedef void (GLAPIENTRY *GLUquadricErrorProc) (GLenum);
+
+typedef void (GLAPIENTRY *GLUtessBeginProc)  (GLenum);
+typedef void (GLAPIENTRY *GLUtessEdgeFlagProc) (GLboolean);
+typedef void (GLAPIENTRY *GLUtessVertexProc) (void *);
+typedef void (GLAPIENTRY *GLUtessEndProc)    (void);
+typedef void (GLAPIENTRY *GLUtessErrorProc)  (GLenum);
+typedef void (GLAPIENTRY *GLUtessCombineProc) (GLdouble[3], void*[4], GLfloat[4], void**);
+typedef void (GLAPIENTRY *GLUtessBeginDataProc)  (GLenum, void *);
+typedef void (GLAPIENTRY *GLUtessEdgeFlagDataProc) (GLboolean, void *);
+typedef void (GLAPIENTRY *GLUtessVertexDataProc) (void *, void *);
+typedef void (GLAPIENTRY *GLUtessEndDataProc)    (void *, void *);
+typedef void (GLAPIENTRY *GLUtessErrorDataProc)  (GLenum, void *);
+typedef void (GLAPIENTRY *GLUtessCombineDataProc) (GLdouble[3], void*[4], GLfloat[4], void**, void *);
+
+typedef void (GLAPIENTRY *GLUnurbsErrorProc) (GLenum);
+
+/*****************************************************************************
+ * GLU enumerants
+ *****************************************************************************/
+
+/* Version */
+#define GLU_VERSION_1_1                 1
+#define GLU_VERSION_1_2                 1
+#define GLU_VERSION_1_3                 1
+
+/* Errors: (return value 0 = no error) */
+#define GLU_INVALID_ENUM        100900
+#define GLU_INVALID_VALUE       100901
+#define GLU_OUT_OF_MEMORY       100902
+#define GLU_INCOMPATIBLE_GL_VERSION     100903
+#define GLU_INVALID_OPERATION   100904
+
+
+/* Boolean */
+#define GLU_FALSE               0
+#define GLU_TRUE                1
+
+/* StringName */
+#define GLU_VERSION             100800
+#define GLU_EXTENSIONS          100801
+
+/* QuadricNormal */
+#define GLU_SMOOTH              100000
+#define GLU_FLAT                100001
+#define GLU_NONE                100002
+
+/* QuadricDrawStyle */
+#define GLU_POINT               100010
+#define GLU_LINE                100011
+#define GLU_FILL                100012
+#define GLU_SILHOUETTE          100013
+
+/* QuadricOrientation */
+#define GLU_OUTSIDE             100020
+#define GLU_INSIDE              100021
+
+/* TessCallback */
+#define GLU_TESS_BEGIN          100100
+#define GLU_VERTEX              100101
+#define GLU_BEGIN               100100
+#define GLU_TESS_VERTEX         100101
+#define GLU_TESS_END            100102
+#define GLU_END                 100102
+#define GLU_TESS_ERROR          100103
+#define GLU_TESS_EDGE_FLAG      100104
+#define GLU_EDGE_FLAG           100104
+#define GLU_TESS_COMBINE        100105
+#define GLU_TESS_BEGIN_DATA     100106
+#define GLU_TESS_VERTEX_DATA    100107
+#define GLU_TESS_END_DATA       100108
+#define GLU_TESS_ERROR_DATA     100109
+#define GLU_TESS_EDGE_FLAG_DATA 100110
+#define GLU_TESS_COMBINE_DATA   100111
+
+/* TessContour */
+#define GLU_CW                  100120
+#define GLU_CCW                 100121
+#define GLU_INTERIOR            100122
+#define GLU_EXTERIOR            100123
+#define GLU_UNKNOWN             100124
+
+/* TessProperty */
+#define GLU_TESS_WINDING_RULE           100140
+#define GLU_TESS_BOUNDARY_ONLY          100141
+#define GLU_TESS_TOLERANCE              100142
+
+/* TessError */
+#define GLU_TESS_ERROR1                 100151
+#define GLU_TESS_ERROR2                 100152
+#define GLU_TESS_ERROR3                 100153
+#define GLU_TESS_ERROR4                 100154
+#define GLU_TESS_ERROR5                 100155
+#define GLU_TESS_ERROR6                 100156
+#define GLU_TESS_ERROR7                 100157
+#define GLU_TESS_ERROR8                 100158
+#define GLU_TESS_MISSING_BEGIN_POLYGON  100151
+#define GLU_TESS_MISSING_BEGIN_CONTOUR  100152
+#define GLU_TESS_MISSING_END_POLYGON    100153
+#define GLU_TESS_MISSING_END_CONTOUR    100154
+#define GLU_TESS_COORD_TOO_LARGE        100155
+#define GLU_TESS_NEED_COMBINE_CALLBACK  100156
+
+/* TessWinding */
+#define GLU_TESS_WINDING_ODD            100130
+#define GLU_TESS_WINDING_NONZERO        100131
+#define GLU_TESS_WINDING_POSITIVE       100132
+#define GLU_TESS_WINDING_NEGATIVE       100133
+#define GLU_TESS_WINDING_ABS_GEQ_TWO    100134
+
+/* NurbsProperty */
+#define GLU_AUTO_LOAD_MATRIX            100200
+#define GLU_CULLING                     100201
+#define GLU_SAMPLING_TOLERANCE          100203
+#define GLU_DISPLAY_MODE                100204
+#define GLU_PARAMETRIC_TOLERANCE        100202
+#define GLU_SAMPLING_METHOD             100205
+#define GLU_U_STEP                      100206
+#define GLU_V_STEP                      100207
+#define GLU_NURBS_MODE                  100160
+#define GLU_NURBS_MODE_EXT              100160
+#define GLU_NURBS_TESSELLATOR           100161
+#define GLU_NURBS_TESSELLATOR_EXT       100161
+#define GLU_NURBS_RENDERER              100162
+#define GLU_NURBS_RENDERER_EXT          100162
+
+/* NurbsSampling */
+#define GLU_OBJECT_PARAMETRIC_ERROR     100208
+#define GLU_OBJECT_PARAMETRIC_ERROR_EXT 100208
+#define GLU_OBJECT_PATH_LENGTH          100209
+#define GLU_OBJECT_PATH_LENGTH_EXT      100209
+#define GLU_PATH_LENGTH                 100215
+#define GLU_PARAMETRIC_ERROR            100216
+#define GLU_DOMAIN_DISTANCE             100217
+
+/* NurbsTrim */
+#define GLU_MAP1_TRIM_2         100210
+#define GLU_MAP1_TRIM_3         100211
+
+/* NurbsDisplay */
+/*      GLU_FILL                100012 */
+#define GLU_OUTLINE_POLYGON     100240
+#define GLU_OUTLINE_PATCH       100241
+
+/* NurbsCallback */
+/*      GLU_ERROR               100103 */
+#define GLU_NURBS_ERROR         100103
+#define GLU_NURBS_BEGIN         100164
+#define GLU_NURBS_BEGIN_EXT     100164
+#define GLU_NURBS_VERTEX        100165
+#define GLU_NURBS_VERTEX_EXT    100165
+#define GLU_NURBS_NORMAL        100166
+#define GLU_NURBS_NORMAL_EXT    100166
+#define GLU_NURBS_COLOR         100167
+#define GLU_NURBS_COLOR_EXT     100167
+#define GLU_NURBS_TEXTURE_COORD 100168
+#define GLU_NURBS_TEX_COORD_EXT 100168
+#define GLU_NURBS_END           100169
+#define GLU_NURBS_END_EXT       100169
+#define GLU_NURBS_BEGIN_DATA    100170
+#define GLU_NURBS_BEGIN_DATA_EXT        100170
+#define GLU_NURBS_VERTEX_DATA   100171
+#define GLU_NURBS_VERTEX_DATA_EXT       100171
+#define GLU_NURBS_NORMAL_DATA   100172
+#define GLU_NURBS_NORMAL_DATA_EXT       100172
+#define GLU_NURBS_COLOR_DATA    100173
+#define GLU_NURBS_COLOR_DATA_EXT        100173
+#define GLU_NURBS_TEXTURE_COORD_DATA    100174
+#define GLU_NURBS_TEX_COORD_DATA_EXT    100174
+#define GLU_NURBS_END_DATA      100175
+#define GLU_NURBS_END_DATA_EXT  100175
+
+/* NurbsError */
+#define GLU_NURBS_ERROR1        100251
+#define GLU_NURBS_ERROR2        100252
+#define GLU_NURBS_ERROR3        100253
+#define GLU_NURBS_ERROR4        100254
+#define GLU_NURBS_ERROR5        100255
+#define GLU_NURBS_ERROR6        100256
+#define GLU_NURBS_ERROR7        100257
+#define GLU_NURBS_ERROR8        100258
+#define GLU_NURBS_ERROR9        100259
+#define GLU_NURBS_ERROR10       100260
+#define GLU_NURBS_ERROR11       100261
+#define GLU_NURBS_ERROR12       100262
+#define GLU_NURBS_ERROR13       100263
+#define GLU_NURBS_ERROR14       100264
+#define GLU_NURBS_ERROR15       100265
+#define GLU_NURBS_ERROR16       100266
+#define GLU_NURBS_ERROR17       100267
+#define GLU_NURBS_ERROR18       100268
+#define GLU_NURBS_ERROR19       100269
+#define GLU_NURBS_ERROR20       100270
+#define GLU_NURBS_ERROR21       100271
+#define GLU_NURBS_ERROR22       100272
+#define GLU_NURBS_ERROR23       100273
+#define GLU_NURBS_ERROR24       100274
+#define GLU_NURBS_ERROR25       100275
+#define GLU_NURBS_ERROR26       100276
+#define GLU_NURBS_ERROR27       100277
+#define GLU_NURBS_ERROR28       100278
+#define GLU_NURBS_ERROR29       100279
+#define GLU_NURBS_ERROR30       100280
+#define GLU_NURBS_ERROR31       100281
+#define GLU_NURBS_ERROR32       100282
+#define GLU_NURBS_ERROR33       100283
+#define GLU_NURBS_ERROR34       100284
+#define GLU_NURBS_ERROR35       100285
+#define GLU_NURBS_ERROR36       100286
+#define GLU_NURBS_ERROR37       100287
+
+/* ------------------------------------------------------------------------- */
+
+#ifdef GLU_MANGLE_H
+#include <GL/glu_mangle.h>
+#endif
+
+GLAPI const GLubyte * GLAPIENTRY gluErrorString (GLenum error);
+
+GLAPI const GLubyte * GLAPIENTRY gluGetString (GLenum name);
+
+GLAPI void GLAPIENTRY gluOrtho2D (GLdouble left, GLdouble right, GLdouble bottom, GLdouble top);
+
+GLAPI void GLAPIENTRY gluPerspective (GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar);
+
+GLAPI void GLAPIENTRY gluPickMatrix (GLdouble x, GLdouble y, GLdouble delX, GLdouble delY, GLint *viewport);
+
+GLAPI void GLAPIENTRY gluLookAt (GLdouble eyex, GLdouble eyey, GLdouble eyez, GLdouble centerx, GLdouble centery, GLdouble centerz, GLdouble upx, GLdouble upy, GLdouble upz);
+
+GLAPI int GLAPIENTRY gluProject (GLdouble objX, GLdouble objY, GLdouble objZ, const GLdouble *model, const GLdouble *proj, const GLint *view, GLdouble* winX, GLdouble* winY, GLdouble* winZ);
+
+GLAPI int GLAPIENTRY gluUnProject (GLdouble winX, GLdouble winY, GLdouble winZ, const GLdouble *model, const GLdouble *proj, const GLint *view, GLdouble* objX, GLdouble* objY, GLdouble* objZ);
+
+GLAPI int GLAPIENTRY gluScaleImage (GLenum format, GLsizei wIn, GLsizei hIn, GLenum typeIn, const void *dataIn, GLsizei wOut, GLsizei hOut, GLenum typeOut, GLvoid* dataOut);
+
+GLAPI int GLAPIENTRY gluBuild1DMipmaps (GLenum target, GLint internalFormat, GLsizei width, GLenum format, GLenum type, const void *data);
+
+GLAPI int GLAPIENTRY gluBuild2DMipmaps (GLenum target, GLint internalFormat, GLsizei width, GLsizei height, GLenum format, GLenum type, const void *data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __glu_h__ */
